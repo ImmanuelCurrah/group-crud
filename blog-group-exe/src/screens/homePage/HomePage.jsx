8d3f07b71a642aed34838e88f330956101f2567b
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-01-28 15:20:34 -0500

    added layout to homepage

--- blog-group-exe/src/screens/homePage/HomePage.jsx
+++ blog-group-exe/src/screens/homePage/HomePage.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Layout from "../../components/layout/Layout";
 
 export default function HomePage() {
-  return <div></div>;
+  return (
+    <div>
+      <Layout>
+        
+      </Layout>
+    </div>
+  );
 }

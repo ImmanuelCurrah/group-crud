bad613b9b0537ed2371d783aa0294de5b3426928
Galina Kishirova <galinakishirova@Galinas-MacBook-Pro.local>
2022-01-28 17:09:45 -0500

    sign up, css in progress

--- blog-group-exe/src/components/signup/SignUp.jsx
+++ blog-group-exe/src/components/signup/SignUp.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import classes from "./SignUp.module.css";
 import { signUpHandler } from '../../services/apiConfig';
 import { existingUsersHandler } from '../../services/apiConfig';
+// import Layout from '../layout/Layout';
 
 export default function SignUp() {
   const [newUser, setNewUser] = useState({
     userName: "",
+    avatar: "",
     firstName: "",
     lastName: "",
     email: "",
@@ -58,34 +60,36 @@ export default function SignUp() {
   }
 
   return (
-    <div>
+    // <Layout>
+    <div className={classes.signUp}>
       <form onSubmit={handleSubmit}>
         <h2 className={classes.title}>Sign up</h2>
         <h3>{validationMessage}</h3>
-        <label>User name</label><br/>
+        <label className={classes.label}>User name</label><br/>
         <input type="text" placeholder='create user name'
         id='userName' value={newUser.userName} onChange={handleInput}/><br/>
-        <label>Upload avatar</label><br/>
+        <label className={classes.label}>Upload avatar</label><br/>
         <input type="text" placeholder='avatar link'
         id='avatar' value={newUser.avatar} onChange={handleInput}/><br/>
-        <label>First Name</label><br/>
+        <label className={classes.label}>First Name</label><br/>
         <input type="text" placeholder='first name'
         id='firstName' value={newUser.firstName} onChange={handleInput}/><br/>
-        <label>Last Name</label><br/>
+        <label className={classes.label}>Last Name</label><br/>
         <input type="text" placeholder='last name'
         id='lastName' value={newUser.lastName} onChange={handleInput}/><br/>
-        <label>Email</label><br/>
+        <label className={classes.label}>Email</label><br/>
         <input type="text" placeholder='email'
         id='email' value={newUser.email} onChange={handleInput}/><br/>
-        <label>Password</label><br/>
+        <label className={classes.label}>Password</label><br/>
         <input type="text" placeholder='create password'
         id='password' value={newUser.password} onChange={handleInput}/><br/>
-        <label>Confirm password</label><br/>
+        <label className={classes.label}>Confirm password</label><br/>
         <input type="text" placeholder='confirm password'
           id='confirmPassword' value={newUser.confirmPassword} onChange={handleInput} /><br/>
         <br />
-        <button disabled={!valid}>SIGH UP</button>
+        <button className={classes.submit} disabled={!valid}>SIGH UP</button>
       </form>
-    </div>
+      </div>
+      // </Layout>
     )
 }

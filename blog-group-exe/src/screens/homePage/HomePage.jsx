230f37aab06de6712533a73a6ce50eda1d4bf108
Immanuel Currah <immanueli.currah@gmail.com>
2022-01-28 20:47:10 -0500

    fixed the mystery of the navbar!

--- blog-group-exe/src/screens/homePage/HomePage.jsx
+++ blog-group-exe/src/screens/homePage/HomePage.jsx
@@ -1,6 +1,7 @@
 // Laura added Layout to homepage
 import React from "react";
 import Layout from "../../components/layout/Layout";
+import SignUp from "../../components/signup/SignUp";
 
 export default function HomePage() {
   return (

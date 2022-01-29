eccf6ab086385c97f4b789f99c4c85fc2adf5298
Virginie Bonhomme <virginiebonhomme@Virginies-MBP.home>
2022-01-29 15:12:35 -0500

    form css

--- blog-group-exe/src/components/login/LoginForm.jsx
+++ blog-group-exe/src/components/login/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import loginformcss from "./LoginForm.module.css";
 import { useState, useEffect } from "react"
 import { loginHandler } from "../../services/apiConfig"
 import Layout from '../layout/Layout';
@@ -28,10 +29,10 @@ function LoginForm(props) {
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await loginHandler(form);
+      await loginHandler(form);
     };
     fetchData();
-  }, []);
+  }, [form]);
 
   const { userName, firstName, lastName, email, password } = props
 
@@ -44,63 +45,76 @@ function LoginForm(props) {
   return (
     <div>
       <Layout />
-      <h3>Login</h3>
-      <form>
-        <label>User Name</label>
-        <br />
-        <input
-          required
-          type="text"
-          name="username"
-          value={userName}
-          placeholder="Username"
-          onChange={handleChange}
-        />
-        <br />
-        <label>First Name</label>
-        <br />
-        <input
-          required
-          type="text"
-          name="firstName"
-          value={firstName}
-          placeholder="First Name"
-          onChange={handleChange}
-        />
-        <br />
-        <label>Last Name</label>
-        <br />
-        <input
-          required
-          type="text"
-          name="lastName"
-          value={lastName}
-          placeholder="Last Name"
-          onChange={handleChange}
-        />
-        <br />
-        <label>Email</label>
-        <br />
-        <input
-          required
-          type="text"
-          name="email"
-          value={email}
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        <br />
-        <label>Password</label>
-        <br />
-        <input
-          required
-          name="password"
-          value={password}
-          placeholder="Password"
-          onChange={handleChange}
-        />
-      </form>
-
+      <div className={loginformcss.back}>
+        <div className={loginformcss.form_body}>
+          <form className={loginformcss.form}>
+            <h3 className={loginformcss.login} >Login</h3>
+            {/* <label className={loginformcss.label_container} >User Name</label> */}
+            <br />
+            <input
+              required
+              className={loginformcss.input_container}
+              type="text"
+              name="username"
+              value={userName}
+              placeholder="Username"
+              onChange={handleChange}
+            />
+            <br />
+            {/* <label className={loginformcss.label_container} >First Name</label> */}
+            <br />
+            <input
+              required
+              className={loginformcss.input_container}
+              type="text"
+              name="firstName"
+              value={firstName}
+              placeholder="First Name"
+              onChange={handleChange}
+            />
+            <br />
+            {/* <label className={loginformcss.label_container} >Last Name</label> */}
+            <br />
+            <input
+              required
+              className={loginformcss.input_container}
+              type="text"
+              name="lastName"
+              value={lastName}
+              placeholder="Last Name"
+              onChange={handleChange}
+            />
+            <br />
+            {/* <label className={loginformcss.label_container}>Email</label> */}
+            <br />
+            <input
+              required
+              className={loginformcss.input_container}
+              type="text"
+              name="email"
+              value={email}
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            <br />
+            {/* <label className={loginformcss.label_container} >Password</label> */}
+            <br />
+            <input
+              required
+              className={loginformcss.input_container}
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              className={loginformcss.btn}
+              type="button"
+              value="Submit" />
+          </form>
+        </div>
+      </div>
     </div>
   )
 }

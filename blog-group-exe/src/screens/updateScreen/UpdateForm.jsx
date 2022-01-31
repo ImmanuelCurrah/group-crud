b8f19813c5c286ae28608bcc141e8bfae28a680b
Immanuel Currah <immanueli.currah@gmail.com>
2022-01-31 15:52:13 -0500

    updated user

--- blog-group-exe/src/screens/updateScreen/UpdateForm.jsx
+++ blog-group-exe/src/screens/updateScreen/UpdateForm.jsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import { userState } from React
-import { updateUserURL, } from "../../services/apiConfig";
+import React from "react";
+import { useState } from "react";
+import Footer from "../../UI/footer/Footer";
+import LoggedInNavBar from "../../UI/loggedInNavBar/LoggedInNavBar";
+import { updateUserHandler } from "../../services/apiConfig";
+import classes from "./UpdateForm.module.css";
+
 function UpdateForm() {
-  const [updatedUser, setUpdatedUser] = useState("")
+  const [updatedUser, setUpdatedUser] = useState({
+    userName: "",
+  });
+
+  const username = localStorage.getItem("username");
+  console.log(username);
 
   const handleChange = (e) => {
-    setUpdatedUser(e.target.value)
-  }
+    const { id, value } = e.target;
+    setUpdatedUser((prevUserName) => ({
+      ...prevUserName,
+      [id]: value,
+    }));
+  };
 
-  return <div>
-    <form>
-      <input
-        className='updateUsername'
-        placeholder='User Name'
-        value={user}
-        name='UserName'
-        required
-        onChange={handleChange}
-      />
-    </form>
-  </div>;
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const newUser = await updateUserHandler(updatedUser, username);
+    console.log(newUser);
+  };
+
+  return (
+    <>
+      <LoggedInNavBar />
+      <div className={classes.container}>
+        <form onSubmit={submitHandler}>
+          <input
+            className="updateUsername"
+            placeholder="User Name"
+            id="userName"
+            value={updatedUser.userName}
+            name="UserName"
+            required
+            onChange={handleChange}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default UpdateForm;
